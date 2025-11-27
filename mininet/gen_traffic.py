13be2621d6ca92789bab97db0511ef99ad6131a0
NhatHoang26122003 <nhathoang261203@gmail.com>
2025-11-27 21:28:22 +0700

    Updated traffic_data_collector & traffic_generator & data

--- mininet/gen_traffic.py
+++ mininet/gen_traffic.py
@@ -11,7 +11,7 @@ from mininet.log import setLogLevel, info
 # --- CẤU HÌNH HỆ THỐNG ---
 CONTROLLER_IP = '127.0.0.1'
 CONTROLLER_PORT = 6633
-TOTAL_DURATION = 300   # Tăng lên 300s (5 phút) để thu thập đủ data
+TOTAL_DURATION = 100   # Tăng thời gian lên để kịp thu thập data
 POLLING_INTERVAL = 2   
 
 # --- CẤU HÌNH MỞ RỘNG ---
@@ -22,11 +22,13 @@ BW_LIMIT = 20
 
 class ExpandedTopo(Topo):
     def build(self):
+        # 1. Tạo 2 Switch gom
         s_src = self.addSwitch('s_src', dpid='1') 
         s_dst = self.addSwitch('s_dst', dpid='2') 
         
         link_opts = dict(bw=BW_LIMIT, delay='5ms', loss=0, max_queue_size=1000, use_htb=True)
 
+        # 2. Tạo Hosts
         for i in range(1, NUM_CLIENTS + 1):
             client = self.addHost(f'h_src_{i}')
             self.addLink(client, s_src, **link_opts)
@@ -35,6 +37,7 @@ class ExpandedTopo(Topo):
             server = self.addHost(f'h_dst_{i}')
             self.addLink(s_dst, server, **link_opts)
 
+        # 3. Tạo các đường đi song song
         for i in range(1, NUM_PATHS + 1):
             dpid_val = str(10 + i) 
             sw_path = self.addSwitch(f's_path_{i}', dpid=dpid_val)
@@ -50,17 +53,10 @@ class TrafficGenerator:
     def generate(self, duration=TOTAL_DURATION):
         info(f"*** Bắt đầu sinh traffic trong {duration}s...\n")
         
-        # --- CẤU HÌNH TRỌNG SỐ (WEIGHTS) ĐỂ CÂN BẰNG DATA ---
-        # Ưu tiên Video/VoIP vì Web thường sinh ra nhiều mẫu hơn dù ít request
-        traffic_types = ['video', 'voip', 'web', 'background']
-        # Tỷ lệ: 35% Video, 35% VoIP, 15% Web, 15% Background
-        weights = [0.35, 0.35, 0.15, 0.15] 
-
         for t in range(duration):
             info(f"--- Cycle {t}/{duration} ---\n")
             for src in self.src_hosts:
-                # Giảm tỷ lệ nghỉ ngơi xuống 10% để sinh nhiều data hơn
-                if random.random() < 0.1: continue
+                if random.random() < 0.3: continue
 
                 target = random.choice(self.dst_hosts)
                 
@@ -69,8 +65,7 @@ class TrafficGenerator:
                 noise = random.uniform(-3, 3)
                 current_bw = max(1, bw_pattern + noise)
 
-                # Chọn loại traffic dựa trên trọng số
-                traffic_type = random.choices(traffic_types, weights=weights, k=1)[0]
+                traffic_type = random.choice(['video', 'voip', 'web', 'background'])
                 
                 if random.random() < 0.05:
                     current_bw += 25
@@ -82,20 +77,16 @@ class TrafficGenerator:
 
     def _send_traffic(self, src, dst, traffic_type, bandwidth):
         target_ip = dst.IP()
-        # Chỉnh lại tham số iperf
+        # Chỉnh lại tham số iperf một chút để đảm bảo chạy ổn định
         if traffic_type == 'video':
             bw_target = bandwidth * 1.2
-            # Video: UDP port 5001
             cmd = f'iperf -c {target_ip} -u -b {bw_target}M -t {POLLING_INTERVAL} -p 5001 &'
         elif traffic_type == 'voip':
             bw_target = 0.5 + (bandwidth * 0.05)
-            # VoIP: UDP port 5002
             cmd = f'iperf -c {target_ip} -u -b {bw_target}M -t {POLLING_INTERVAL} -p 5002 &'
         elif traffic_type == 'web':
-            # Web: TCP port 80
             cmd = f'iperf -c {target_ip} -t {POLLING_INTERVAL} -p 80 &'
         else: 
-            # Background: UDP port 5003
             bw_target = bandwidth * 0.5
             cmd = f'iperf -c {target_ip} -u -b {bw_target}M -t {POLLING_INTERVAL} -p 5003 &'
         
@@ -110,18 +101,22 @@ def run():
     try:
         net.start()
         
-        # --- BẬT STP ĐỂ THU THẬP DỮ LIỆU AN TOÀN ---
+        # --- CẤU HÌNH QUAN TRỌNG: BẬT STP ---
         info("*** Enabling STP on all switches to prevent Loops...\n")
         for s in net.switches:
+            # Bật OpenFlow 1.3
             s.cmd(f'ovs-vsctl set Bridge {s.name} protocols=OpenFlow13')
+            # Bật STP
             s.cmd(f'ovs-vsctl set Bridge {s.name} stp_enable=true')
         
-        info("*** Waiting 45s for STP convergence...\n")
+        info("*** Waiting 45s for STP convergence (DO NOT SKIP)...\n")
+        # Phải chờ STP hội tụ, nếu không pingAll sẽ fail
         time.sleep(45)
         
-        # info("*** Pinging all to learn MAC addresses...\n")
-        # net.pingAll()
+        info("*** Pinging all to learn MAC addresses...\n")
+        net.pingAll()
         
+        # --- KHỞI ĐỘNG SERVER ---
         info(f"*** Starting iperf servers...\n")
         dst_hosts = [net.get(f'h_dst_{i}') for i in range(1, NUM_SERVERS + 1)]
         for h in dst_hosts:
